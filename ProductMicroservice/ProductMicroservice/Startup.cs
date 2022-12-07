3a5265cd2b7819dbbba5cb09868ccce104ab0126
Mehrdad <mehzan07@yahoo.com>
2022-12-07 14:37:30 +0100

    Swagger should be out of Dev Environment, should be load in both production  and Dev environment

--- ProductMicroservice/ProductMicroservice/Startup.cs
+++ ProductMicroservice/ProductMicroservice/Startup.cs
@@ -97,13 +97,15 @@ namespace ProductMicroservice
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
-                    c.RoutePrefix = string.Empty;
-                });
             }
+            // Swagger should be out of Dev Environment, should be load in both production  and Dev environment
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseHttpsRedirection();
             app.UseRouting();
 
